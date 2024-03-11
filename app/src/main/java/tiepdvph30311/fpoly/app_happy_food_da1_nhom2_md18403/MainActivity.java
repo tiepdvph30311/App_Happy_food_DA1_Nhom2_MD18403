e06cee92fca821c6e260dc66eaab8d205dfcffd0
tiepdvph30311 <dinhvantiep48@gmail.com>
2024-03-11 15:04:57 +0700

    first commit

---
+++ app/src/main/java/tiepdvph30311/fpoly/app_happy_food_da1_nhom2_md18403/MainActivity.java
@@ -0,0 +1,14 @@
+package tiepdvph30311.fpoly.app_happy_food_da1_nhom2_md18403;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
